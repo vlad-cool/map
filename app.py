53128efbcd513c5c3c6160daffcf7af775d387f4
vlad-cool <vlad-matveev2004@mail.ru>
2021-01-14 23:52:29 +0300

    hz, robit li, prosto tak delau comit

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, flash, redirect, url_for, request, jsonify, send_from_directory
+import math
 import json
 
 app = Flask(__name__)
@@ -7,11 +8,34 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+def proj_cyll_p(points, scale):
+    for i in len(points):
+        points[i] = math.sin(points[i] * math.pi / 180*60*60*100) * scale
+    return points
+
+def proj_cyll_l(points, start, scale):
+    for i in len(points):
+        points[i] = points[i] * scale
+    return points
+
 @app.route('/getmap/<map>')
 def get_map(map):
     map_dat = json.load(open('maps/' + map + '.json', 'r'))
     print(map_dat)
-    return render_template("map.html")
+    polyline = open('maps/' + map_dat["points"]).read()
+    polyline = list(map(int, polyline.split()))
+    pointsP = polyline[0::2]  #Широта
+    pointsL = polyline[1::2]  #Долгота
+    print(pointsP)
+    print(pointsL)
+
+    proj_cyll_p(pointsP, 1)
+    proj_cyll_l(pointsL, 0, 1)
+
+    polyline[0::2] = pointsP
+    polyline[1::2] = pointsL
+
+    return render_template("map.html", polyline = polyline)
 
 if __name__ == '__main__':
     app.run()

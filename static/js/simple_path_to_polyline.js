75b159f39a9bd92cad6bd7094c93091ae1c4294c
vlad-cool <vlad-matveev2004@mail.ru>
2021-04-08 07:53:02 +0300

    it seems it working

--- static/js/simple_path_to_polyline.js
+++ static/js/simple_path_to_polyline.js
@@ -34,30 +34,95 @@ function pol_from_path(path)
     
     var paths = input_svg.getElementsByTagName("path");
     var html_paths = document.getElementById("input_view").getElementsByTagName("path");
+    var d = "";
 
-    for (var i = 0; i < paths.length; i++)
+    var i = 0;
+    while (html_paths.length > 0)
     {
-        var d = paths[i].getAttribute("d").substr(2);
-        console.log(d);
-        
         var pol = "";
+        var pols = [];
         var cx = 0.0, cy = 0.0;
 
-        d = d.replaceAll(',', ' ');
+        d = paths[i].getAttribute("d").replaceAll(',', ' ').trim().substr(1).trim();
 
         while (d.length > 0)
         {
+            if (d[0] == "m")
+            {
+                pols.push(pol);
+                //console.log(pol);
+                //console.log("12321");
+                pol = "";
+                d = d.substr(1).trim();
+            }
+
+            if (d[0] == "z")
+            {
+                pol += " " + pol.split(" ")[0];
+                console.log(pol);
+                d = d.substr(1).trim();
+
+                cx = parseFloat(pol.split(",")[0]);
+                cx = parseFloat(pol.split(",")[1]);
+                continue;
+            }
+
+            /*
+            console.log("###")
+            console.log(d);
+            console.log(pol);
+            console.log("###" + cx + " " + cy + "###");
+            
+
+            if (isNaN(cx))
+            {
+                //console.log(d);
+                //console.log(pol);
+                var answer = prompt('question', 'defaultAnswer');
+            }
+            */
+
             cx += parseFloat(d);
-            d = removeFloat(d).substr(1);
+            d = removeFloat(d).substr(1).trim();
             cy += parseFloat(d);
-            d = removeFloat(d).substr(1);
-            pol += cx + "," + cy + " "; 
+            d = removeFloat(d).substr(1).trim();
+            pol += cx + "," + cy + " ";
         }
 
-        html_paths[0].removeAttribute("d");
-        html_paths[0].setAttribute("points", pol);
-        html_paths[0].outerHTML = html_paths[0].outerHTML.replaceAll("path", "polyline");
+        pols.push(pol);
+        //console.log(pol);
+
+        //console.log(pols);
+
+        for (var j = 0; j < pols.length; j++)
+        {
+            var polyline = document.createElement("polyline");
+
+            {
+                function cloneAttributes(element, sourceNode) {
+                    let attr;
+                    let attributes = Array.prototype.slice.call(sourceNode.attributes);
+                    while(attr = attributes.pop()) {
+                        element.setAttribute(attr.nodeName, attr.nodeValue);
+                    }
+                }
+
+                cloneAttributes(polyline, html_paths[0]);
+            }
+
+            console.log(polyline);
+
+            polyline.removeAttribute("d");
+            polyline.removeAttribute("id");
+            polyline.setAttribute("points", pols[j]);
+
+            html_paths[0].parentElement.appendChild(polyline);
+        }
+        
+
+        html_paths[0].parentNode.removeChild(html_paths[0]);
         html_paths = document.getElementById("input_view").getElementsByTagName("path");
+        i++;
     }
 }
 

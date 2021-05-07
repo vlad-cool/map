17121f33c5b754980b04d50880572d8c71c9fd2b
vlad-cool <vlad-matveev2004@mail.ru>
2021-05-07 18:26:22 +0300

    something works

--- shp.py
+++ shp.py
@@ -3,27 +3,35 @@ import math
 import random
 from turtle import Screen, Turtle
 
+r = 1000
+
+def proj(point):
+    x = point[0] * r / 180 + r
+    y = -point[1] * r / 180 + r
+
+    #y = r * math.tan(math.pi * -point[1] / 180) + 800
+
+    return x, y
+
 sf = shapefile.Reader("resources/shape/ne_10m_admin_0_countries_lakes/ne_10m_admin_0_countries_lakes")
 
 f = open('out.txt','w')
 svg = open('out.svg','w')
 
-svg.write('<svg width="1600" height="1600" xmlns="http://www.w3.org/2000/svg">')
+###svg.write('<svg width="1600" height="1600" xmlns="http://www.w3.org/2000/svg">')
 
 shapes = sf.shapes()
 
-r = 400
 
 ###screen = Screen()
 ### screen.setup(2 * r + 4, 2 * r + 8)  # fudge factors due to window borders & title bar
 ###turtle = Turtle()
 ###turtle.penup()
 
-r = 200
 
 counter = 1
 
-for k in sf.shapes():
+'''for k in sf.shapes():
     if counter > 0:
         counter -= 1
     else:
@@ -67,20 +75,32 @@ for k in sf.shapes():
 
 
 #turtle.exitonclick()
+'''
+
+colors = ["red", "green", "blue", "cyan", "gray"]
+shapes = sf.shapes()
+
+svg.write('<svg width="' + str(2 * r) + '" height="' + str(2 * r) + '" xmlns="http://www.w3.org/2000/svg">')
+
+for shape in shapes:
+    color = colors[random.randrange(0, len(colors), 1)]
+
+    parts = shape.parts
+
+    for i in range(len(shape.points)):
+        if len(parts) > 0 and i == parts[0]:
+            if parts[0] != 0:
+                svg.write('" />\n')
+            svg.write('<polygon stroke="' + colors[random.randrange(0, len(colors), 1)] + '" stroke-width="1px" fill="none" points="')
+            parts = parts[1::]
+
+        point = shape.points[i]
+        x, y = proj(point)
+        svg.write(" " + str(x) + "," + str(y))
+    
+    svg.write('" />\n')
+
 svg.write('</svg>')
-#f.write("#########################\n")
-#
-#fields = sf.fields
-#
-#for i in fields:
-#    f.write(str(i) + "\n")
-#
-#f.write("#########################\n")
-#
-#records = sf.records()
-#
-#for i in records:
-#    f.write(str(i) + "\n")
 
 sf.close()
 f.close()

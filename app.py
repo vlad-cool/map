5f8f3ff37906d5646f892c9ac763d4bd5a6e0b67
vlad-cool <vlad-matveev2004@mail.ru>
2021-05-10 23:15:26 +0300

    Chto-to zdelal

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
 import os
+import shp
 from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
+    return render_template("index.html  ")
+
     return render_template("simple_path_to_polyline.html")
 
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static', 'favicons'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+@app.route('get_map')
+def get_map():
+    
 
 if __name__ == "__main__":
     app.run()
--- shp.py
+++ shp.py
@@ -1,6 +1,10 @@
 import shapefile
 import math
 import random
+import json
+import pickle
+import simplejson
+
 from turtle import Screen, Turtle
 
 r = 1000
@@ -13,86 +17,77 @@ def proj(point):
 
     return x, y
 
-sf = shapefile.Reader("resources/shape/ne_10m_admin_0_countries_lakes/ne_10m_admin_0_countries_lakes")
+'''def parse_record(record):
+    
+    #attrs = vars(record)
+    #f.write(str(attrs) + "\n")
 
-f = open('out.txt','w')
-svg = open('out.svg','w')
+    try:
+        attrs = vars(record)["_Record__field_positions"]
+    except:
+        attrs = vars(record)
 
-###svg.write('<svg width="1600" height="1600" xmlns="http://www.w3.org/2000/svg">')
+    for attr_name in tuple(attrs.keys()):
+        #f.write(attr_name + " " + str(attrs[attr_name]) + "\n")
+        try:
+            attrs[attr_name] = record[attrs[attr_name]]
+        except:
+            attrs[attr_name] = attrs[attr_name]
 
-shapes = sf.shapes()
+    f.write(str(attrs) + "\n")
 
+    return attrs
 
-###screen = Screen()
-### screen.setup(2 * r + 4, 2 * r + 8)  # fudge factors due to window borders & title bar
-###turtle = Turtle()
-###turtle.penup()
+    try:
+        for attr_name in tuple(attrs.keys()):
+            attrs[attr_name] = eval("record." + attr_name)
+    except:
+        attrs = vars(record)
+        for attr_name in tuple(attrs.keys()):
+            attrs[attr_name] = eval("record." + attr_name)
 
-
-counter = 1
-
-'''for k in sf.shapes():
-    if counter > 0:
-        counter -= 1
-    else:
-        break
-    for i in k.parts:
-        colors = ["red", "green", "blue", "cyan", "gray"]
-
-        f.write(str(i.points) + "\n")
-
-        first = i.points[0]
-
-        y0 = first[1] * r / 180 + 800
-        #####y0 = r * math.tan(math.pi * first[1] / 180) + 800
-        x0 = first[0] * r / 180 + 800
-        ###turtle.color(colors[random.randrange(0, len(colors), 1)])
-        ###turtle.goto(x0, y0)
-        ###turtle.pendown()
-
-        svg.write('<polyline stroke="' + colors[random.randrange(0, len(colors), 1)] + '" stroke-width="1px" fill="none" points="')
-        #svg.write(" " + str(x0) + "," + str(y0))
-
-
-        for a, b in i.points:
-            #print(str(a) + " " + str(b))
-            #x = 400 + tg(j.first)
-
-            y = b * r / 180 + 800
-            #####y = r * math.tan(math.pi * b / 180) + 800
-            x = a * r / 180 + 800
-            #print(x, y)
-            ###turtle.goto(x, y)
-
-            svg.write(" " + str(x) + "," + str(y))
-
-        ###turtle.goto(x0, y0)
-        ###turtle.penup()
-
-        svg.write(" " + str(x0) + "," + str(y0))
-        svg.write('" />\n')
-
-
-
-#turtle.exitonclick()
+    try:
+        return attrs["_Record__field_positions"]
+    except:
+        return attrs
 '''
 
+sf = shapefile.Reader("resources/shape/ne_10m_admin_0_countries_lakes/ne_10m_admin_0_countries_lakes")
+
+f = open('out.txt','w', encoding='utf-8')
+svg = open('out.svg','w', encoding='utf-8')
+
 colors = ["red", "green", "blue", "cyan", "gray"]
-shapes = sf.shapes()
+shapeRecords = sf.shapeRecords()
 
 svg.write('<svg width="' + str(2 * r) + '" height="' + str(2 * r) + '" xmlns="http://www.w3.org/2000/svg">')
 
-for shape in shapes:
-    color = colors[random.randrange(0, len(colors), 1)]
+while len(shapeRecords) > 0:
+    shapeRecord = shapeRecords[0]
+    shapeRecords = shapeRecords[1::]
+    shape = shapeRecord.shape
+    record = shapeRecord.record
 
+    color = colors[random.randrange(0, len(colors), 1)]
     parts = shape.parts
+
+    #try:
+    #print(parse_record(record))
+    ##f.write(str(parse_record(record)))
+    ##print(parse_record(record))
+    ##print(str(parse_record(record)["NAME"]))
+    #except:
+    #    print("ERR")
+    counter = 0
 
     for i in range(len(shape.points)):
         if len(parts) > 0 and i == parts[0]:
             if parts[0] != 0:
                 svg.write('" />\n')
-            svg.write('<polygon stroke="' + colors[random.randrange(0, len(colors), 1)] + '" stroke-width="1px" fill="none" points="')
+            asasasasa = record[17]
+            svg.write('<polygon stroke="' + color + '" id="' + record[17] + str(counter) + '" stroke-width="1px" fill="none" points="')
             parts = parts[1::]
+            counter += 1
 
         point = shape.points[i]
         x, y = proj(point)
@@ -102,5 +97,68 @@ for shape in shapes:
 
 svg.write('</svg>')
 
+################
+#print(str(vars(sf.records()[0])))
+
+#for key, value in vars(sf.records()).iteritems():
+#    f.write(str(key) + ":" + str(value) + "---")
+    
+
+'''
+records = sf.records()
+
+while len(records) > 0:
+    record = records[0]
+    records = records[1::]
+
+    #for name in dir(record):
+    #    if not name.startswith('__'):
+    #        print(name)
+    
+    print(record[3])
+
+    parse_record(record)
+    try:
+        print(str((record.NAME)))
+    except:
+        pass
+'''
+################
+
 sf.close()
 f.close()
+
+def map(attrs):
+    res = ""
+    attrs = attrs.split(" ")
+    sf = shapefile.Reader(attrs[0])
+
+    colors = ["red", "green", "blue", "cyan", "gray"]
+    shapeRecords = sf.shapeRecords()
+    
+    while len(shapeRecords) > 0:
+        shapeRecord = shapeRecords[0]
+        shapeRecords = shapeRecords[1::]
+        shape = shapeRecord.shape
+        record = shapeRecord.record
+
+        color = colors[random.randrange(0, len(colors), 1)]
+        parts = shape.parts
+
+        counter = 0
+
+        for i in range(len(shape.points)):
+            if len(parts) > 0 and i == parts[0]:
+                if parts[0] != 0:
+                    svg.write('" />\n')
+                asasasasa = record[17]
+                res += '<polygon stroke="' + color + '" id="' + record[17] + str(counter) + '" stroke-width="1px" fill="none" points="'
+                parts = parts[1::]
+                counter += 1
+
+            point = shape.points[i]
+            x, y = proj(point)
+            res += " " + str(x) + "," + str(y)
+        res += '" />\n'
+    sf.close()
+    f.close()

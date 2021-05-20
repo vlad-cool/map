22c7dd09d477658474c866fb67f48ace7804771d
vlad-cool <vlad-matveev2004@mail.ru>
2021-05-20 10:21:48 +0300

    removed selectors

--- test.py
+++ test.py
@@ -5,11 +5,14 @@ nam111 = "cities"
 sf = shapefile.Reader("resources/shape/" + nam111 + "/" + nam111)
 
 shapes = sf.shapeRecords()
+out = open("out.txt", "w", encoding="utf-8")
 
-print(sf.shapes()[0].shapeType)
-print(sf.fields)
+records = sf.records()
 
-for shapeRecord in shapes:
-    for i in sf.fields:
-        print(sf.fields, end=" ")
-    print("")
+#print(sf.shapes()[0].shapeType)
+#print(sf.fields)
+
+for record in records:
+    for i in range(len(list(record))):
+        out.write(str(i) + ":" + str(record[i]) + " " * (64 - len(str(i) + ":" + str(record[i]))))
+    out.write("\n")
